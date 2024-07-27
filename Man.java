766a3b76294354da786fed2fe016ecac03a38243
nishaanth-reddy <nishunishanthreddy2026@gmail.com>
2024-07-27 21:20:21 +0530

    Add files via upload

---
+++ Man.java
@@ -0,0 +1,16 @@
+public class Man{
+public static void main(String[] args)
+{
+int time =22;
+if(time<10){
+System.out.println("Good day.");
+}
+else if(time<20)
+{System.out.println("Good day.");
+}
+else{
+System.out.println("Good evening.");
+}
+}
+}
+
