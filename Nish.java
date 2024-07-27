42047111bf2d3bc479f2fd9cce509f3eaacfa1b9
nishaanth-reddy <nishunishanthreddy2026@gmail.com>
2024-07-27 21:31:42 +0530

    Add files via upload

---
+++ Nish.java
@@ -0,0 +1,13 @@
+import java.util.scanner;
+ class Nish
+{
+public static void main(Strring[] args)
+{
+ScannermyObj=new Scanner(System.in);
+String userName;
+
+System.out.println("Enter username");
+userName = myObj.nextline();
+System.out.println("Username is: "+userName);
+}
+}
